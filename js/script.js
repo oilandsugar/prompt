7e1e947a21d6b4e24e1ad36d006ece563786ed3e
oilandsugar <tommy_pm@hotmail.com>
2017-05-21 10:43:31 -0400

    fix timing

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ jQuery(function ($) {
     var playing = false;
     var font_size = $('#font-size').val();
     var height = 10000;
+    var scrollbar_width = $('.up').width();
 
     // controls
 
@@ -28,30 +29,31 @@ jQuery(function ($) {
 
     // scroll zone
 
-    $(".up").hover(function(e) {
-      $(this).mousemove(function (e) {
-        var parentOffset = $(this).parent().offset();
-        var relX = e.pageX - parentOffset.left;
-          console.log(relX);
+    $(".up").hover(function (e) {
+        $(this).mousemove(function (e) {
+          var parentOffset = $(this).parent().offset();
+          var relX = e.pageX - parentOffset.left;
+          console.log('up ' + relX);
           playing = true;
-          scroll(relX);
-      });
-    }, function (e) {
+          scroll(relX, 'up');
+        });
+      }, function (e) {
         playing = false;
         stop();
         console.log('stop playing');
       }
     );
 
-    $(".down").hover(function(e) {
-      $(this).mousemove(function (e) {
-        var parentOffset = $(this).parent().offset();
-        var relX = e.pageX - parentOffset.left;
-        console.log(relX);
-        playing = true;
-        scroll(relX);
-      });
-    }, function (e) {
+    $(".down").hover(function (e) {
+        $(this).mousemove(function (e) {
+          var parentOffset = $(this).parent().offset();
+          var relX = (e.pageX - parentOffset.left) * 5 - 1500;
+          var down = 4000 - (relX);
+          console.log('down ' + ' ' +  relX + ' ' +  down);
+          playing = true;
+          scroll(down, 'down');
+        });
+      }, function (e) {
         playing = false;
         stop();
         console.log('stop playing');
@@ -61,14 +63,10 @@ jQuery(function ($) {
     function scroll(speed, direction) {
       console.log('start scrolling');
       var calc_speed = height / font_size * speed;
-      if(calc_speed !== height / font_size) {
-        $('.scrollWrapper').stop().animate({scrollTop: height}, calc_speed, "linear", function () {
-
-        });
-      } else {
-        $('.scrollWrapper').stop().animate({scrollTop: height}, 1000, "linear", function () {
-
-        });
+      if (direction === 'down') {
+        $('.scrollWrapper').stop().animate({scrollTop: height}, calc_speed, "linear");
+      } else if (direction === 'up') {
+        $('.scrollWrapper').stop().animate({scrollTop: 0}, calc_speed, "linear");
       }
     }
 
